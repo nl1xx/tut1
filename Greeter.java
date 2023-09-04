a2f72dc8ccb742fbf63a2e5c832e82e6da943fa5
nl1xx <2441447381@qq.com>
2023-09-04 10:15:12 +0800

    Added feature 3

--- Greeter.java
+++ Greeter.java
@@ -5,6 +5,7 @@ public class Greeter {
 
   private Locale locale;
   private String name;
+  private int number;
 
   public Greeter(String language, String country, String name) {
     locale = new Locale(language, country);
